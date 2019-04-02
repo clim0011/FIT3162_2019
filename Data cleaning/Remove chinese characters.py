b021271e99647c3452a6aae060d5a5b98f67c717
Run Xun <46264359+runxunteh@users.noreply.github.com>
2019-04-02 16:00:26 +0800

    Update Remove chinese characters.py
    Remove the whole row that contains non-english characters

--- Data cleaning/Remove chinese characters.py
+++ Data cleaning/Remove chinese characters.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 df=pd.read_csv("FinalData.csv")
+print(len(df))
 
 #Remove non-english characters
-df['comments'] = df['comments'].str.replace(r'[^\x00-\x7F]+', '')
+df=df[~df['comments'].str.contains(r'[^\x00-\x7F]+')]
+print(len(df))
 
 df.to_csv('FinalData.csv',index = False)
 
