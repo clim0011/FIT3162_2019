e5e0e03daf7efc94a235c448dc7f9c7d8a763b8c
clim0011 <clim0011@student.monash.edu>
2019-04-05 17:22:10 +0800

    Code to find synonyms of a word

---
+++ Data processing/synonyms.py
@@ -0,0 +1,16 @@
+import nltk
+nltk.download('wordnet')
+nltk.download('stopwords')
+from nltk.corpus import wordnet
+synonyms = []
+antonyms = []
+
+for syn in wordnet.synsets("active"):
+    
+    for l in syn.lemmas():
+        synonyms.append(l.name())
+        if l.antonyms():
+            antonyms.append(l.antonyms()[0].name())
+            
+print(set(synonyms))
+print(set(antonyms))
