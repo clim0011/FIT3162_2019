c716e4dcba954f849c0786919027b6d605e138d9
Run Xun <46264359+runxunteh@users.noreply.github.com>
2019-05-27 23:50:19 +0800

    Added more features
    Feature 1: Add picture for each hotel
    Feature 2: Collapsible section
    Feature 3: Drop down list for 4 groups (Couple, Family, Solo and group)

--- Recommendation system/create_html.py
+++ Recommendation system/create_html.py
@@ -1,5 +1,6 @@
+#Import panda and google_images_download library
 import pandas as pd
-
+from google_images_download import google_images_download  
 
 def create_html(output,dataset):
 
@@ -13,13 +14,17 @@ def create_html(output,dataset):
     family=txt[1]
     solo=txt[2]
     group=txt[3]
-    hotel=txt[4:len(txt)]
+    hotel_score=txt[4:len(txt)]
     #print(hotel)
     #couple[2] for austria hotel should give us 5
     #print(couple[2])
-
+    
     file=pd.read_csv(dataset)
     hotel=file["Hotel_Name"]
+    hotel_address=file["Hotel_Address"]
+    negative_review=file["Negative_Review"]
+    positive_review=file["Positive_Review"]
+    nationality=file["Reviewer_Nationality"]
     unique_hotel=hotel.unique()
     #print(hotel[1])
     couple_hotel=[]
@@ -29,12 +34,26 @@ def create_html(output,dataset):
             couple_hotel.append(hotel_name)
     #print(couple_hotel)
     #print(hotel[int(couple[1])])
+
+    # creating object 
+    response = google_images_download.googleimagesdownload()
+    search_queries=""
+    for i in range(3):
+        if i==2:
+            search_queries+=unique_hotel[i]
+        else:
+            search_queries+=unique_hotel[i]+","    
+
+    arguments = {"keywords": search_queries, "format": "jpg", "limit":1, "no_download":True}
+    paths=response.download(arguments)
+    #print(paths)
+    #print(paths[0]["Hotel Bellevue Wien"])
     page=output.replace("txt","html")
     #print(page)
-    f = open(page,'r')
-    for line in f:
-        print("hi")
-        print(line)
+    #f = open(page,'r')
+    #for line in f:
+        #print("hi")
+        #print(line)
     f = open("testing.html",'w')
     message = """<!--
     Author: Teh Run Xun
@@ -85,7 +104,27 @@ def create_html(output,dataset):
         cursor: pointer;
     }
 
+    .collapsible {
+      position: absolute;
+      right: 0;
+      width: 1700px;
+      height: 200px;
+      border: 3px solid black;
+      background-color: white;
+      color: black;
+      cursor: pointer;
+      padding: 20px;
+      text-align: left;
+      outline: none;
+      font-size: 18px;
+    }
 
+    .content {
+        padding: 0 18px;
+        display: none;
+        overflow: hidden;
+        background-color: white;
+        border: 2px solid black
     </style>
 
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -98,14 +137,34 @@ def create_html(output,dataset):
         <li><a href="Business.html">Business</a></li>
         <li><a href="Leisure.html">Leisure</a></li>
     </ul>
-    <button id="Couple">Couple</button><button id="Family">Family</button><button id="Solo">Solo</button><button id="Group">Group</button>
     </br>
     </br>
-    <div id="Couple_hotel"></div>
-    
     
 """
-    message+='<script>document.getElementById("Couple").onclick=function() {document.getElementById("Couple_hotel").innerHTML='
+    for i in range(3):
+        message+='<p></p><img src='
+        name=unique_hotel[i]
+        for l in range(len(hotel_score)):
+            if name in hotel_score[l]:
+                first_id=int(hotel_score[l][1])
+                score=hotel_score[l][3]
+                break
+        message+='"'+paths[0][name][0]+'" width=200 height=200><button class="collapsible"><p>'
+        message+="Hotel Name: "+name+"</br>Address: "+hotel_address[first_id]+"</br>Score: "+score+"</p></button>"
+        message+='''<div class="content">
+            <p>
+          <select onchange="myFunction(this)">
+          <option></option>
+          <option>Couple</option>
+          <option>Family</option>
+          <option>Solo</option>
+          <option>Group</option>
+         </select>
+
+         <p id="demo'''
+        message+=str(i)+'">Please select one of the option</p></p></div>'
+        
+    """
     for i in range(len(couple_hotel)):
         if i == 0:
             message+='"'
@@ -113,12 +172,74 @@ def create_html(output,dataset):
             message+=couple_hotel[i]+'"'
         else:
             message+=couple_hotel[i]+"<br><br>"
-    message+="; }</script>"
-    message+="<a href="
-    
-    message+="</a>"
+
     message+="<p>"+str(couple[1:5])+"</p>"
-    message+="</body>"
+    """
+    message+='''<script>
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+    function myFunction(selTag) {
+      var x = selTag.options[selTag.selectedIndex].text;
+      if (x=="Couple") {
+            document.getElementById("demo0").innerHTML = "Hotel 1";'''
+    """
+    for i in range(len(unique_hotel)):
+        name=unique_hotel[i]
+        first_id=int(hotel_score[i][1])
+        last_id=int(hotel_score[i][2])
+        x=1
+        add=False
+        while x>=1:
+            index=int(couple[x])
+            print(index)
+            if index>=first_id and index<=last_id:
+                if add==True:
+                    message+="</br><hr>"
+                else:
+                    message+="Reviewer's nationality: " + nationality[i] + "</br></br>Positive: " +  positive_review[i] + "</br></br>Negative:" + negative_review[i]
+                    add=True
+                    x+=1
+            else:
+                message+='</p>";'
+                break"""
+                
+    message+='''
+      }
+      else if (x=="Family") {
+            document.getElementById("demo0").innerHTML = "Hotel 2";
+      }
+      else if (x=="Solo") {
+            document.getElementById("demo0").innerHTML = "Hotel 3";
+      }
+      else if (x=="Group") {
+            document.getElementById("demo0").innerHTML = "Hotel 4";
+      }
+      else if (x=="") {
+            document.getElementById("demo0").innerHTML = "Hotels";'''
+    '''
+    for i in range(len(hotel)):
+        if i==len(hotel)-1:
+            message+="Reviewer's nationality: " + nationality[i] + "</br></br>Positive: " +  positive_review[i] + "</br></br>Negative:" + negative_review[i] + '</p>";'
+        else:
+            message+="Reviewer's nationality: " + nationality[i] + "</br></br>Positive: " +  positive_review[i] + "</br></br>Negative:" + negative_review[i] + "</br><hr>"
+    '''
+    message+="""
+      }
+    }
+    </script>"""
+    message+="</body></html>"
     
     f.write(message)
     f.close()
